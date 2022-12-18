a43df67fbed3958aaba3c8a4cd202db0fc60a733
maurice-cubicit <maurice@cubicit.co.za>
2022-12-18 17:09:44 +0200

    fix: slow loading image

--- components/navigation/Nav.tsx
+++ components/navigation/Nav.tsx
@@ -66,6 +66,7 @@ export default function Nav() {
                 <Image
                   src={CompanyLogo}
                   objectFit={'cover'}
+                  priority={true}
                   alt="company logo"/>
               </div>
             </Link>
